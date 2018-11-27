64c94dfcb9132bf443fea655169af8fdca5b7a60
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-11-27 12:11:31 +0800

    WIP: Add svm. Give up

--- code/py_scripts/color_histogram_helper.py
+++ code/py_scripts/color_histogram_helper.py
@@ -3,21 +3,23 @@ import itertools
 import cv2
 import numpy as np
 import pandas as pd
-from config import TRAIN_PATH
+from config import TRAIN_PATH, TEST_PATH
 
 channels = ["red", "green", "blue", "yellow"]
 num_class = 28
 
 def get_ids(path):
-    return list(set(f.split('_')[0] for f in os.listdir(path)))
+    return list(set(f.split('_')[0] for f in os.listdir(path) if f.endswith("png")))
 
 def hist_intersection(hist1, hist2):
     minima = np.minimum(hist1, hist2)
-    intersection = np.true_divide(np.sum(minima), np.sum(hist2))
+    with np.errstate(divide="ignore", invalid="ignore"):
+        intersection = np.true_divide(np.sum(minima), np.sum(hist2))
+        intersection = np.nan_to_num(intersection)
     return intersection
 
 def get_intersections(id_):
-    imgs = [cv2.imread(os.path.join(TRAIN_PATH, "{}_{}.png".format(id_, channel))) for channel in channels]
+    imgs = [cv2.imread(os.path.join(TEST_PATH, "{}_{}.png".format(id_, channel))) for channel in channels]
     hists = [cv2.calcHist([img], [0], None, [256], [0,256]) for img in imgs]
     return [hist_intersection(*pair) for pair in itertools.combinations(hists, 2)]
 
@@ -43,6 +45,7 @@ def to_category(list_, max_=num_class):
     for x in list_:
         category[int(x)] = 1
     return category
+
 
 if __name__ == '__main__':
     train = get_train()
