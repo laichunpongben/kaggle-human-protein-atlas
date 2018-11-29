b71b8d7600b8150cbe6d81fe5947cef99460aa69
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-11-29 16:58:45 +0800

    Customize data bunch for multilabel

--- code/cnn.py
+++ code/cnn.py
@@ -1,46 +1,31 @@
 import os
 import numpy as np
 import pandas as pd
-from fastai.vision import ImageDataBunch
-from fastai.vision.transform import rand_resize_crop
+from fastai.vision import ImageItemList
+from fastai.vision.transform import get_transforms
 from fastai.vision.learner import create_cnn
 from fastai.vision.models import resnet18
 from fastai.metrics import accuracy
-from config import TRAIN_PATH, TEST_PATH
 
 num_class = 28
 PATH = "data/rgb"
 
-def to_category(list_, max_=num_class):
-    category = [0] * max_
-    for x in list_:
-        category[int(x)] = 1
-    return category
-
-
-train = pd.read_csv(os.path.join(TRAIN_PATH, "../train.csv"))
-# get y values
-train['Target'] = train['Target'].str.split(' ')
-train['Categories'] = list(train['Target'].map(to_category))
-y_columns = list(range(num_class))
-train[y_columns] = pd.DataFrame(train.Categories.values.tolist(), index=train.index)
-train = train.drop(['Target', 'Categories'], axis=1)
-
-# tfms = rand_resize_crop(24)
-# print(tfms)
-data = ImageDataBunch.from_df("data/rgb/train",
-                              train,
-                              suffix=".png",
-                              fn_col="Id",
-                              label_col=list(range(num_class)),
-                              size=24,
-                              bs=16)  # df_tfms=tfms,
-
+tfms = get_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.)
+stats = ([0.08069, 0.05258, 0.05487], [0.13704, 0.10145, 0.15313])
+data = (ImageItemList.from_csv(PATH, 'train.csv', folder="train", suffix='.png')
+                     .random_split_by_pct()
+                     .label_from_df(sep=' ')
+                     .transform(tfms, size=24)
+                     .databunch()
+                     .normalize(stats))
 
 learner = create_cnn(data, resnet18, metrics=accuracy)
-learner.fit(1)
-learner.save('one_epoch')
+learner.fit_one_cycle(5,1e-2)
+learner.save('mini_train')
 
-img = learner.data.train_ds[0][0]
-result = learner.predict(img)
-print(result)
+learner.show_results(figsize=(12,15))
+
+#
+# img = learner.data.train_ds[0][0]
+# result = learner.predict(img)
+# print(result)
