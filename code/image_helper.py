40e219a529e637f61bf9bee2854d68addb2586c9
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-07 12:03:37 +0800

    Refactor get_mask
    Former-commit-id: e9dc7b902ee2319780b983a2f7710cb04b79aa2c

--- code/image_helper.py
+++ code/image_helper.py
@@ -6,10 +6,12 @@ from scipy import ndimage as ndi
 import skimage
 from skimage import data
 from skimage.morphology import watershed, remove_small_objects
-from skimage.filters import sobel
+from skimage.filters import sobel, gaussian
 from skimage.color import label2rgb
 from skimage.exposure import adjust_gamma
 import matplotlib.pyplot as plt
+
+CHANNELS = ['red', 'blue', 'green', 'yellow']
 
 
 def get_ids(path):
@@ -24,31 +26,42 @@ def merge_rgb(id, input_path, output_path):
     img = Image.fromarray(rgb)
     img.save(os.path.join(output_path, "{}.png".format(id)))
 
-def get_mask(img):
-    img = adjust_gamma(img, gamma=0.5)  # arbitrary
+def get_mask(img, channel):
+    assert channel in CHANNELS
+
+    # (exposure gamma, gaussian filter sigma, object min size, erosion iteration, dilation iteration)
+    # arbitrary
+    params = {
+        "red": (0.5, 3.0, 64, 0, 0),
+        "green": (0.5, 2.0, 64, 0, 0),
+        "blue": (0.5, 0.0, 16, 0, 0),
+        "yellow": (0.5, 2.0, 64, 0, 0)
+    }
+    gamma, sigma, min_size, erosion, dilation = params.get(channel, (0,0,0,0,0))
+
+    if gamma > 0:
+        img = adjust_gamma(img, gamma=gamma)
+    if sigma > 0:
+        ima = gaussian(img, sigma=sigma) * 255
 
     elevation_map = sobel(img)
     markers = np.zeros_like(img)
     markers[img < 30] = 1
     markers[img > 150] = 2
 
-    # print(markers)
-
     segmentation = watershed(elevation_map, markers)
     segmentation = ndi.binary_fill_holes(segmentation - 1)
-    segmentation = remove_small_objects(segmentation, 16)  # arbitrary
-    segmentation = ndi.binary_dilation(segmentation, iterations=20)
+
+    if min_size > 0:
+        segmentation = remove_small_objects(segmentation, min_size)
+    if erosion > 0:
+        segmentation = ndi.binary_erosion(segmentation, iterations=erosion)
+    if dilation > 0:
+        segmentation = ndi.binary_dilation(segmentation, iterations=dilation)
+
     labeled_coins, _ = ndi.label(segmentation)
-    # print(labeled_coins)
-
-    # fig, ax = plt.subplots(figsize=(4, 3))
-    # ax.imshow(segmentation, cmap=plt.cm.gray, interpolation='nearest')
-    # ax.set_title('segmentation')
-    # ax.axis('off')
-
 
     num_coin = labeled_coins.max()
-    print(num_coin)
     mask = []
     for i in range(1, num_coin+1):  # 0 is black
         m = np.where(labeled_coins==i, 1, 0)
@@ -58,31 +71,33 @@ def get_mask(img):
         mask.append(empty)
     mask = np.stack(mask, axis=-1)
 
-
-    # plt.tight_layout()
-    #
-    # plt.show()
-    #
     return mask
 
 def mask2img(mask):
     return np.max(mask, axis=-1) * 255
 
-
-if __name__ == '__main__':
-    # input_path = "data/official/test"
-    # output_path = "data/rgb/test"
-    # ids = get_ids(input_path)
-    # print(len(ids))
-    # for id_ in ids:
-    #     merge_rgb(id_, input_path, output_path)
-
+def official_to_mask_png(channel):
     dataset_dir = "data/official/train"
-    mask_dir = "data/dilated_mask"
     ids = get_ids(dataset_dir)
+    mask_dir = "data/{}_mask".format(channel)
+    if not os.path.exists(mask_dir):
+        os.mkdir(mask_dir)
+
     for id_ in sorted(ids):
-        img = skimage.io.imread(os.path.join(dataset_dir, "{}_blue.png".format(id_)))
-        mask = get_mask(img)
+        img = skimage.io.imread(os.path.join(dataset_dir, "{}_{}.png".format(id_, channel)))
+        mask = get_mask(img, channel)
         print(id_, mask.shape)
         img_mask = mask2img(mask)
-        skimage.io.imsave(os.path.join(mask_dir, "{}_mask.png".format(id_)), img_mask)
+        skimage.io.imsave(os.path.join(mask_dir, "{}_{}_mask.png".format(id_, channel)), img_mask)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='valid arguments')
+    parser.add_argument('channel', type=str, help='color channel')
+
+    args = parser.parse_args()
+    channel = args.channel
+    assert channel in CHANNELS
+
+    official_to_mask_png(channel)
