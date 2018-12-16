f1e9206090419d0fd6442fd2c9e983b3fcdb28bf
ccneko <clairechung112@gmail.com>
2018-12-16 12:38:38 +0800

    add encoder depth option
    Former-commit-id: 79a047bc2436f6a404c68b87729259e06c220307

--- code/resnet_fastai.py
+++ code/resnet_fastai.py
@@ -18,7 +18,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i","--gpuid", help="GPU device id", type=int, choices=range(8), default=0)
 parser.add_argument("-s","--imagesize", help="image size", type=int, default=256)
 parser.add_argument("-b","--batchsize", help="batch size (not in use yet)", type=int, default=64)
-parser.add_argument("-d","--dropout", help="dropout (float)", type=float, default=0.5)
+parser.add_argument("-d","--encoderdepth", help="encoder depth of the network", type=int, choices=[34,50,101,152], default=152)
+parser.add_argument("-p","--dropout", help="dropout (float)", type=float, default=0.5)
 parser.add_argument("-e","--epochnum1", help="epoch number for stage 1", type=int, default=25)
 parser.add_argument("-E","--epochnum2", help="epoch number for stage 2", type=int, default=50)
 args = parser.parse_args()
@@ -27,7 +28,7 @@ torch.cuda.set_device(args.gpuid)
 bs = args.batchsize
 dropout = args.dropout
 size = args.imagesize
-runname=str(size)+'drop'+str(dropout)+'-ep'+str(args.epochnum1)+'_'+str(args.epochnum2)
+runname='rn'+str(args.encoderdepth)+'-'+str(size)+'-drop'+str(dropout)+'-ep'+str(args.epochnum1)+'_'+str(args.epochnum2)
 
 
 num_class = 28
@@ -50,14 +51,13 @@ src.add_test(test_fnames, label='0')
 src.test.x.create_func = open_4_channel
 src.test.x.open = open_4_channel
 
-
 trn_tfms,_ = get_transforms(do_flip=True, flip_vert=True, max_rotate=30., max_zoom=1,
                       max_lighting=0.05, max_warp=0.)
 data = (src.transform((trn_tfms, _), size=size)
         .databunch().normalize(protein_stats))
 
-def resnet50(pretrained):
-    return Resnet4Channel(encoder_depth=50)
+def resnet(pretrained):
+    return Resnet4Channel(encoder_depth=args.encoderdepth)
 
 # copied from https://github.com/fastai/fastai/blob/master/fastai/vision/learner.py
 def _resnet_split(m): return (m[0][6],m[1])
@@ -66,7 +66,7 @@ f1_score = partial(fbeta, thresh=0.2, beta=1)
 
 learn = create_cnn(
     data,
-    resnet50,
+    resnet,
     cut=-2,
     split_on=_resnet_split,
     ps=dropout,
@@ -79,13 +79,13 @@ learn = create_cnn(
 # learn.recorder.plot()
 lr = 3e-2
 learn.fit_one_cycle(args.epochnum1, slice(lr))
-learn.save('stage-1-rn50-'+runname)
+learn.save('stage-1-'+runname)
 learn.unfreeze()
 # learn.lr_find()
 # learn.recorder.plot()
 learn.fit_one_cycle(args.epochnum2, slice(3e-5, lr/args.epochnum2))
-learn.save('stage-2-rn50'+runname)
+learn.save('stage-2-'+runname)
 preds,_ = learn.get_preds(DatasetType.Test)
 pred_labels = [' '.join(list([str(i) for i in np.nonzero(row>0.1)[0]])) for row in np.array(preds)]
 df = pd.DataFrame({'Id':test_ids,'Predicted':pred_labels})
-df.to_csv(out_path/'rn50'+runname+'.csv', header=True, index=False)
+df.to_csv(out_path+runname+'.csv', header=True, index=False)
