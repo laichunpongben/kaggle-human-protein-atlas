a2e95c146a22a4d929286049a1965c2bd8e8573d
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-17 15:19:06 +0800

    Update func doc
    Former-commit-id: fc7963821c2c86dc1d1e8c63a1269e58dacc7f54

--- code/loss.py
+++ code/loss.py
@@ -3,6 +3,12 @@ import torch.nn.functional as F
 
 
 def focal_loss(input, target, gamma=2):
+    '''
+    https://arxiv.org/abs/1708.02002
+
+    With gamma=0, focal loss is equivalent to binary cross entropy loss.
+    https://gombru.github.io/2018/05/23/cross_entropy_loss/
+    '''
     if not (target.size() == input.size()):
         raise ValueError("Target size ({}) must be the same as input size ({})"
                          .format(target.size(), input.size()))
