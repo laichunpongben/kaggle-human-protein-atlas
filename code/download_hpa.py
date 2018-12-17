0333ed9190ffe6902d695007b37beb617ca72a03
ccneko <clairechung112@gmail.com>
2018-12-18 02:39:52 +0800

    code: download_hpa - download all colors, mv dir to data
    Former-commit-id: ef57ec2fd4fbae422d2763124c61c4d523714c28

--- code/download_hpa.py
+++ code/download_hpa.py
@@ -5,19 +5,22 @@ from pathlib import Path
 from PIL import Image
 from io import BytesIO
 
-df = pd.read_csv('../HPAv18RBGY_wodpl.csv')
+df = pd.read_csv('HPAv18RBGY_wodpl.csv')
+colors = ['blue','red','green','yellow']
 
 def download(id_):
-    try:
-        hpa_dir = Path('../HPAv18')
-        hpa_dir.mkdir(parents=True, exist_ok=True)
-        image_dir, _, image_id = id_.partition('_')
-        url = f'http://v18.proteinatlas.org/images/{image_dir}/{image_id}_blue_red_green_yellow.jpg'
-        r = requests.get(url)
-        image = Image.open(BytesIO(r.content)).resize((512, 512), Image.LANCZOS)
-        image.save(hpa_dir / f'{id_}.png', format='png')
-    except:
-        print(f'{id_} broke...')
+    for color in colors:
+        try:
+            hpa_dir = Path('data/HPAv18')
+            hpa_dir.mkdir(parents=True, exist_ok=True)
+            image_dir, _, image_id = id_.partition('_')
+            url = f'http://v18.proteinatlas.org/images/{image_dir}/{image_id}_{color}.jpg'
+            print(url)
+            r = requests.get(url)
+            image = Image.open(BytesIO(r.content)).resize((512, 512), Image.LANCZOS)
+            image.save(hpa_dir / f'{id_}_{color}.png', format='png')
+        except:
+            print(f'{id_}_{color} broke...')
 
 p = Pool()
 p.map(download, df.Id)
