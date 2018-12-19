51aef18f70e09b87f1aba03351b3c3b8138072b1
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-19 09:35:38 +0800

    Add check exist for download
    Former-commit-id: 3688af3c559012ae94a9e381c733235afdc9c4ee

--- code/download_hpa.py
+++ code/download_hpa.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import requests
 from multiprocessing import Pool
@@ -7,20 +8,24 @@ from io import BytesIO
 
 df = pd.read_csv('data/HPAv18RBGY_wodpl.csv')
 colors = ['blue','red','green','yellow']
+hpa_dir = Path('/media/ben/LENOVO_USB_HDD/HPAv18_train')
+hpa_dir.mkdir(parents=True, exist_ok=True)
+exist_ids = sorted([f.split('.png')[0] for f in os.listdir(hpa_dir) if f.endswith('.png')])
 
 def download(id_):
     for color in colors:
-        try:
-            hpa_dir = Path('data/HPAv18')
-            hpa_dir.mkdir(parents=True, exist_ok=True)
-            image_dir, _, image_id = id_.partition('_')
-            url = f'http://v18.proteinatlas.org/images/{image_dir}/{image_id}_{color}.jpg'
-            print(url)
-            r = requests.get(url)
-            image = Image.open(BytesIO(r.content)).resize((512, 512), Image.LANCZOS).convert('L')
-            image.save(hpa_dir / f'{id_}_{color}.png', format='png')
-        except:
-            print(f'{id_}_{color} broke...')
+        if '_'.join([id_, color]) in exist_ids:
+            print('{}_{} Already exist'.format(id_, color))
+        else:
+            try:
+                image_dir, _, image_id = id_.partition('_')
+                url = f'http://v18.proteinatlas.org/images/{image_dir}/{image_id}_{color}.jpg'
+                print(url)
+                r = requests.get(url)
+                image = Image.open(BytesIO(r.content)).resize((512, 512), Image.LANCZOS).convert('L')
+                image.save(hpa_dir / f'{id_}_{color}.png', format='png')
+            except:
+                print(f'{id_}_{color} broke...')
 
 p = Pool()
 p.map(download, df.Id)
