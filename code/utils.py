500c1a2347ab1fb47768806702295db30b86db03
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-24 15:49:24 +0800

    Add debug open time
    Former-commit-id: 7cb18a50ee151cede25b09bad02c0090863baad6

--- code/utils.py
+++ code/utils.py
@@ -13,10 +13,12 @@ from .image_service import clipped_zoom
 nuclei_count_density = get_nuclei_count_density()
 debug_counter = 0
 debug_time = time.time()
+open_time = 0.0
 
 # adapted from https://www.kaggle.com/iafoss/pretrained-resnet34-with-rgby-0-460-public-lb
 def open_4_channel(fname):
-    global debug_counter, debug_time
+    global debug_counter, debug_time, open_time
+    start = time.time()
     fname = str(fname)
     # strip extension before adding color
     if fname.endswith('.png'):
@@ -43,10 +45,12 @@ def open_4_channel(fname):
     #     # if zoom out, padding is handled
     #     x = clipped_zoom(x, zoom_scale)
     img = Image(pil2tensor(x, np.float32).float())
+    now = time.time()
+    open_time += now - start
     if debug_counter % 100 == 0:
-        now = time.time()
-        print(now - debug_time)
+        print(now - debug_time, open_time, open_time/(now - debug_time))
         debug_time = now
+        open_time = 0.0
     debug_counter += 1
 
     return img
