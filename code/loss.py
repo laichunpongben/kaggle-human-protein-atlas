d8b8262877b8cccafa5358288c08f9a56350a6fa
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-26 05:49:08 +0800

    Revert focal loss
    Former-commit-id: ce43f793e22439139a0a548726369069834ee32b

--- code/loss.py
+++ code/loss.py
@@ -12,48 +12,48 @@ def focal_loss(input, target, gamma=2):
     With gamma=0, focal loss is equivalent to binary cross entropy loss.
     https://gombru.github.io/2018/05/23/cross_entropy_loss/
     '''
-    # if not (target.size() == input.size()):
-    #     raise ValueError("Target size ({}) must be the same as input size ({})"
-    #                      .format(target.size(), input.size()))
+    if not (target.size() == input.size()):
+        raise ValueError("Target size ({}) must be the same as input size ({})"
+                         .format(target.size(), input.size()))
+
+    max_val = (-input).clamp(min=0)
+    loss = input - input * target + max_val + \
+        ((-max_val).exp() + (-input - max_val).exp()).log()
+
+    invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
+    loss = (invprobs * gamma).exp() * loss
+
+    return loss.sum(dim=1).mean()
+
+    # num_class = 28
+    # size_average = True
+    # gamma = 2.0
     #
-    # max_val = (-input).clamp(min=0)
-    # loss = input - input * target + max_val + \
-    #     ((-max_val).exp() + (-input - max_val).exp()).log()
+    # N = input.size(0)
+    # C = input.size(1)
+    # P = F.sigmoid(input)
     #
-    # invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
-    # loss = (invprobs * gamma).exp() * loss
+    # class_mask = input.data.new(N, C).fill_(0)
+    # class_mask = Variable(class_mask)
+    # ids = target.view(-1, 1)
+    # class_mask.scatter_(1, ids.data, 1.)
     #
-    # return loss.sum(dim=1).mean()
-
-    num_class = 28
-    size_average = True
-    gamma = 2.0
-
-    N = input.size(0)
-    C = input.size(1)
-    P = F.sigmoid(input)
-
-    class_mask = input.data.new(N, C).fill_(0)
-    class_mask = Variable(class_mask)
-    ids = target.view(-1, 1)
-    class_mask.scatter_(1, ids.data, 1.)
-
-    alpha = Variable(torch.ones(num_class, 1))
-    if input.is_cuda and not alpha.is_cuda:
-        alpha = alpha.cuda()
-    alpha = alpha[ids.data.view(-1)]
-
-    probs = (P*class_mask).sum(1).view(-1,1)
-
-    log_p = probs.log()
-
-    batch_loss = -alpha*(torch.pow((1-probs), gamma))*log_p
-
-    if size_average:
-        loss = batch_loss.mean()
-    else:
-        loss = batch_loss.sum()
-    return loss
+    # alpha = Variable(torch.ones(num_class, 1))
+    # if input.is_cuda and not alpha.is_cuda:
+    #     alpha = alpha.cuda()
+    # alpha = alpha[ids.data.view(-1)]
+    #
+    # probs = (P*class_mask).sum(1).view(-1,1)
+    #
+    # log_p = probs.log()
+    #
+    # batch_loss = -alpha*(torch.pow((1-probs), gamma))*log_p
+    #
+    # if size_average:
+    #     loss = batch_loss.mean()
+    # else:
+    #     loss = batch_loss.sum()
+    # return loss
 
 
 def f1_loss(input, target, threshold=0.5):
