f100dfe8e9cb7b7f97b7bd8b9b7de3178544dcd3
Ben Lai <ben.c.p.lai@hsbc.com.hk>
2018-12-28 23:12:50 +0800

    Add pred to ensemble
    Former-commit-id: 19340aca399837b1340a127373459b1d18d56d07

--- code/csv_service.py
+++ code/csv_service.py
@@ -1,5 +1,10 @@
+import os
 from collections import defaultdict
+from pathlib import Path
+import torch
 import pandas as pd
+import numpy as np
+from config import PRED_PATH, OUT_PATH, DATASET_PATH
 
 
 def get_annotations(train_csv):
@@ -74,18 +79,57 @@ def vote(list_, min_vote, first_n):
     labels = sorted([k for k, v in d.items() if v>=min_vote], key=lambda x: -d[x])[:first_n]
     return ' '.join(labels)
 
-if __name__ == '__main__':
+def get_preds(runname, fold):
+    all_preds = []
+    for i in range(fold):
+        path = Path(PRED_PATH)/f'{runname}-{i}.pth'
+        preds = torch.load(path)
+        all_preds.append(preds)
+    return all_preds
 
-    csvs = [
-        "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep15_25.csv",  # 0.465
-        "output/resnet50-512-official_hpav18-bce-weighted-drop0.5-th0.1-bs16-lr0.005-ep5_15.csv",  # 0.465
-        "output/resnet50-512-official-bce-weighted-drop0.5-th0.1-bs16-lr0.005-ep5_15.csv",  # 0.460
-        "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep5_15.csv",  # 0.458
-        "output/resnet50-512-bce-weighted-drop0.5-th0.1-bs16-lr0.01-ep5_15.csv",  # 0.455
-        "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep25_50.csv",  # 0.443
-        "output/resnet50-256-official_hpav18-bce-random-drop0.5-th0.1-bs64-lr0.01-ep5_15.csv"  # 0.415
-    ]
-    out = "output/ensemble_0_1_2_3_4_5_6_vote4.csv"
+def output_csv_avg(runname, fold, th):
+    preds = get_preds(runname, fold)
+    preds = torch.stack(preds)
+    preds = torch.mean(preds, dim=0)
+    pred_labels = [' '.join(list([str(i) for i in np.nonzero(row>th)[0]])) for row in np.array(preds)]
+
+    test_ids = list(sorted({fname.split('_')[0] for fname in os.listdir(Path(DATASET_PATH)/'test') if fname.endswith('.png')}))
+    df = pd.DataFrame({'Id':test_ids,'Predicted':pred_labels})
+    out_file = Path(OUT_PATH)/f'{runname}-avg-th{th}.csv'
+    df.to_csv(out_file, header=True, index=False)
+
+def output_csv_vote(runname, fold, th, min_vote, first_n=99):
+    test_ids = list(sorted({fname.split('_')[0] for fname in os.listdir(Path(DATASET_PATH)/'test') if fname.endswith('.png')}))
+
+    preds = get_preds(runname, fold)
+    csvs = []
+    for i in range(fold):
+        pred_labels = [' '.join(list([str(i) for i in np.nonzero(row>th)[0]])) for row in np.array(preds[i])]
+        df = pd.DataFrame({'Id':test_ids,'Predicted':pred_labels})
+        out_file = Path(OUT_PATH)/f'{runname}-th{th}-{i}.csv'
+        csvs.append(out_file)
+        df.to_csv(out_file, header=True, index=False)
+
+    vote_out = Path(OUT_PATH)/f'{runname}-vote{min_vote}-th{th}.csv'
+    ensemble(csvs, vote_out, min_vote, first_n)
+
+if __name__ == '__main__':
+    runname = "resnet50-512-official-bce-random-drop0.5-th0.1-bs32-lr0.01-ep5_15"
+    fold = 5
+    th = 0.2
     min_vote = 4
-    first_n = 99
-    ensemble(csvs, out, min_vote, first_n)
+    output_csv_vote(runname, fold, th, min_vote)
+
+    # csvs = [
+    #     "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep15_25.csv",  # 0.465
+    #     "output/resnet50-512-official_hpav18-bce-weighted-drop0.5-th0.1-bs16-lr0.005-ep5_15.csv",  # 0.465
+    #     "output/resnet50-512-official-bce-weighted-drop0.5-th0.1-bs16-lr0.005-ep5_15.csv",  # 0.460
+    #     "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep5_15.csv",  # 0.458
+    #     "output/resnet50-512-bce-weighted-drop0.5-th0.1-bs16-lr0.01-ep5_15.csv",  # 0.455
+    #     "output/resnet50-512-bce-random-drop0.5-th0.1-bs16-lr0.01-ep25_50.csv",  # 0.443
+    #     "output/resnet50-256-official_hpav18-bce-random-drop0.5-th0.1-bs64-lr0.01-ep5_15.csv"  # 0.415
+    # ]
+    # out = "output/ensemble_0_1_2_3_4_5_6_vote4.csv"
+    # min_vote = 4
+    # first_n = 99
+    # ensemble(csvs, out, min_vote, first_n)
